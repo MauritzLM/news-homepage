e6adfe2197517c461688bbe577c7bda17d428939
Mauritz Marais <mauritz303@gmail.com>
2022-12-05 15:12:19 +0200

    updated readme, erased unnecessary code

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
+// click event on close button to remove focus
 (function () {
     const closeMenuBtn = document.querySelector('.close-menu-btn');
-    // const menu = document.querySelector('.main-navigation');
-    // const header = document.querySelector('header');
 
-    closeMenuBtn.addEventListener('click', (e) => {
+    closeMenuBtn.addEventListener('click', () => {
         // remove focus from button
         closeMenuBtn.blur();
     });
