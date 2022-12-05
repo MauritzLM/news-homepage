10ecacb923447dd47f4963b2320154182468e4c2
Mauritz Marais <mauritz303@gmail.com>
2022-12-05 14:53:46 +0200

    updated desktop styles

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// select close button
-// select menu
-// onclick change menu styles
 
-const closeMenuBtn = document.querySelector('.close-menu-btn');
-const menu = document.querySelector('.main-navigation');
-const header = document.querySelector('header');
+(function () {
+    const closeMenuBtn = document.querySelector('.close-menu-btn');
+    // const menu = document.querySelector('.main-navigation');
+    // const header = document.querySelector('header');
 
-closeMenuBtn.addEventListener('click', (e) => {
-    // remove focus from button
-    closeMenuBtn.blur();
-});
+    closeMenuBtn.addEventListener('click', (e) => {
+        // remove focus from button
+        closeMenuBtn.blur();
+    });
+})();
+
